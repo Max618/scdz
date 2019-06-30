c6f8ced8ba9494fa18681fe6f04fd08d55a81cfe
matheus9bh@hotmail.com <matheus9bh@hotmail.com>
2019-06-30 19:52:29 -0300

    fix-login-screen

--- src/LoginView.java
+++ src/LoginView.java
@@ -18,10 +18,22 @@ public class LoginView extends JFrame {
 	}
 
 	private void button1MouseClicked(MouseEvent e) {
-		JOptionPane.showMessageDialog(null,"Bem vindo ao SCDZ");
-        IndexView frame = new IndexView();
-        frame.setVisible(true);
-        this.setVisible(false);
+		
+		 if(txtlogin.getText().equals("admin@scdz.com.br") && (String.valueOf(txtsenha.getPassword()).equals("admin"))){
+				JOptionPane.showMessageDialog(null,"Bem vindo ao SCDZ");
+			 IndexView frame = new IndexView();
+		        frame.setVisible(true);
+		        this.setVisible(false);
+		    }    
+		        else{
+		                    JOptionPane.showMessageDialog(null, "Email ou senha incorretos");
+		                    txtsenha.setText("");
+		        }
+		
+		
+		
+		}
+		
 		/*Employee em = new Employee();
 		Boolean x = em.Login(txtemail.getText(), txtsenha.getText());
 		if(x == true) {
@@ -33,12 +45,16 @@ public class LoginView extends JFrame {
 			txtsenha.setText("");
 		}*/
 			 
-	}
+	
 
 	private void initComponents() {
 		// JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
 		// Generated using JFormDesigner Evaluation license - MATHEUS CANDIDO CARVALHO
 		button1 = new JButton();
+		txtlogin = new JTextField();
+		label1 = new JLabel();
+		label2 = new JLabel();
+		txtsenha = new JPasswordField();
 
 		//======== this ========
 		Container contentPane = getContentPane();
@@ -52,20 +68,47 @@ public class LoginView extends JFrame {
 			}
 		});
 
+		//---- label1 ----
+		label1.setText("E-mail");
+
+		//---- label2 ----
+		label2.setText("Senha");
+
 		GroupLayout contentPaneLayout = new GroupLayout(contentPane);
 		contentPane.setLayout(contentPaneLayout);
 		contentPaneLayout.setHorizontalGroup(
 			contentPaneLayout.createParallelGroup()
-				.add(GroupLayout.TRAILING, contentPaneLayout.createSequentialGroup()
-					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-					.add(button1, GroupLayout.PREFERRED_SIZE, 165, GroupLayout.PREFERRED_SIZE)
-					.add(8, 8, 8))
+				.add(contentPaneLayout.createSequentialGroup()
+					.addContainerGap()
+					.add(contentPaneLayout.createParallelGroup()
+						.add(GroupLayout.TRAILING, contentPaneLayout.createSequentialGroup()
+							.add(contentPaneLayout.createParallelGroup(GroupLayout.TRAILING)
+								.add(txtlogin, GroupLayout.DEFAULT_SIZE, 300, Short.MAX_VALUE)
+								.add(button1, GroupLayout.DEFAULT_SIZE, 300, Short.MAX_VALUE))
+							.add(8, 8, 8))
+						.add(contentPaneLayout.createSequentialGroup()
+							.add(label1)
+							.addContainerGap(277, Short.MAX_VALUE))
+						.add(contentPaneLayout.createSequentialGroup()
+							.add(txtsenha, GroupLayout.DEFAULT_SIZE, 298, Short.MAX_VALUE)
+							.addContainerGap())
+						.add(contentPaneLayout.createSequentialGroup()
+							.add(label2)
+							.add(0, 278, Short.MAX_VALUE))))
 		);
 		contentPaneLayout.setVerticalGroup(
 			contentPaneLayout.createParallelGroup()
-				.add(contentPaneLayout.createSequentialGroup()
-					.addContainerGap()
-					.add(button1, GroupLayout.DEFAULT_SIZE, 126, Short.MAX_VALUE)
+				.add(GroupLayout.TRAILING, contentPaneLayout.createSequentialGroup()
+					.add(14, 14, 14)
+					.add(label1)
+					.addPreferredGap(LayoutStyle.RELATED)
+					.add(txtlogin, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.add(23, 23, 23)
+					.add(label2)
+					.addPreferredGap(LayoutStyle.RELATED)
+					.add(txtsenha, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(LayoutStyle.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+					.add(button1, GroupLayout.PREFERRED_SIZE, 55, GroupLayout.PREFERRED_SIZE)
 					.addContainerGap())
 		);
 		pack();
@@ -76,5 +119,9 @@ public class LoginView extends JFrame {
 	// JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
 	// Generated using JFormDesigner Evaluation license - MATHEUS CANDIDO CARVALHO
 	private JButton button1;
+	private JTextField txtlogin;
+	private JLabel label1;
+	private JLabel label2;
+	private JPasswordField txtsenha;
 	// JFormDesigner - End of variables declaration  //GEN-END:variables
 }
